ea1a03df933d2ef9ed73c31e829604cf344f936a
hm0524 <38096718+hm0524@users.noreply.github.com>
2018-10-19 11:44:15 +0900

    Add files via upload

--- data/setting.js
+++ data/setting.js
@@ -1,7 +1,7 @@
 const seting = [
 	{
 		"id": 1,
-		"menuTitle": "いざゆけ!難田学園中等部",
+		"menuTitle": "いざゆけ!難田学園中等部(外部リンク)",
 		"pickUpUrl": "img/nanda_header.png",
 		"workUrl": "http://cre-m.jp/category/gif_manga/nanda_gakuen/",
 		"pickUpTitle": "いざゆけ!<br>難田学園中等部",
@@ -12,45 +12,45 @@ const seting = [
 		"id": 2,
 		"menuTitle": "マイスタイル",
 		"pickUpUrl": "",
-		"workUrl": "../mystyle/index.html",
-		"pickUpTitle": "タイトル02",
+		"workUrl": "../mystyle/mystyle.html",
+		"pickUpTitle": "マイスタイル",
 		"caption": "キャプション02",
 		"comment": "作品02のコメント"
 	},
 	{
 		"id": 3,
-		"menuTitle": "作品03",
-		"pickUpUrl": "img/j0040.png",
-		"workUrl": "sub02.html",
-		"pickUpTitle": "マンガ",
-		"caption": "キャプション03キャプション",
+		"menuTitle": "おめかしスタイル",
+		"pickUpUrl": "img/j0006.jpg",
+		"workUrl": "../omekashi/omekashi.html",
+		"pickUpTitle": "おめかし<br>スタイル",
+		"caption": "特別な日にも使えて<br>デイリーコーデにも使える♪",
 		"comment": "作品03のコメント"
 	},
 	{
 		"id": 4,
-		"menuTitle": "作品04",
-		"pickUpUrl": "img/j0006.jpg",
-		"workUrl": "img/j0006.jpg",
-		"pickUpTitle": "おめかし<br>スタイル",
-		"caption": "特別な日にも使えて<br>デイリーコーデにも使える♪",
+		"menuTitle": "ウィッグの上手なかぶり方",
+		"pickUpUrl": "img/j0032.png",
+		"workUrl": "../wig/wig.html",
+		"pickUpTitle": "HOW TO WEAR",
+		"caption": "-ウィッグの上手なかぶり方-",
 		"comment": "作品04のコメント"
 	},
 	{
 		"id": 5,
-		"menuTitle": "作品05",
-		"pickUpUrl": "img/j0032.png",
-		"workUrl": "img/j0032.png",
-		"pickUpTitle": "HOW TO WEAR",
-		"caption": "-ウィッグの上手なかぶり方-",
+		"menuTitle": "GIFステッカー",
+		"pickUpUrl": "img/j0048.gif",
+		"workUrl": "../gifstacker/gifstacker.html",
+		"pickUpTitle": "gif<br>アニメ",
+		"caption": "GIF<br>ステッカー",
 		"comment": "作品05のコメント"
 	},
 	{
 		"id": 6,
-		"menuTitle": "作品06",
-		"pickUpUrl": "img/j0048.gif",
-		"workUrl": "img/j0048.gif",
-		"pickUpTitle": "gif<br>アニメ",
-		"caption": "キャプション06<br>キャプション",
+		"menuTitle": "縦読み漫画",
+		"pickUpUrl": "img/j0040.png",
+		"workUrl": "../comic/comic.html",
+		"pickUpTitle": "縦読み漫画",
+		"caption": "縦読み漫画",
 		"comment": "作品06のコメント"
 	}
 ]
